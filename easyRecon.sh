06c5af5789f4a32f9d23b45fc939ad0aa260591b
whit3 h4t <59191514+cspshivam@users.noreply.github.com>
2020-12-03 04:07:15 +0530

    Update easyRecon.sh

--- easyRecon.sh
+++ easyRecon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "   
+echo -e "\e[96m   
           ======  ======   ========  \       /
 	  |      |      |        /    \     /
 	  |      |      |       /      \   /
@@ -11,53 +11,48 @@ echo "
          
                                         by @cspshivam
                                         https://www.cspshivam.com
-                                                      	  "
+                                                      	\e[0m"
 domain=$1
 if [[ -z $domain ]]; then
 	echo -e "Usage: ./easyRecon.sh <domain.com>"
 	exit 1
 fi
-echo "[-] EasyRecon will gather the following information for you
-      [+] All existing domains of $domain
-      [+] All Live domains of $domain
-      [+] All URLs/Links of $domain
-      [+] All JS files of $domain
-                                               "
+echo -e "\e[91m[-] EasyRecon will gather the following information for you\e[0m"
+echo -e "\e[34m[+]\e[0mAll existing domains of $domain"
+echo -e "\e[34m[+]\e[0mAll Live domains of $domain"
+echo -e "\e[34m[+]\e[0mAll URLs/Links of $domain"
+echo -e "\e[34m[+]\e[0mAll JS files of $domain"
 echo "	"
-echo " This will take some time. You can have a cup of coffee"
+echo -e "\e[41m This will take some time. You can have a cup of coffee \e[0m"
 echo "	"
 echo "	"
 
 mkdir $domain
 
-echo "Enumerating subdomains with SubFinder..."
+echo -e "\e[93mEnumerating subdomains with SubFinder...\e[0m"
 subfinder -d $domain -silent > $domain/all-domains.txt
 
 echo $domain >> $domain/all-domains.txt
-echo "[- Task Done -] All domains of $domain are saved in all-domains.txt"
+echo -e "[- Task Done -] \e[92mAll domains of $domain are saved in all-domains.txt\e[0m"
 
 sleep 1 # wait for 1 second
 echo "	"
-echo "Checking for LIVE domains...
-	This will take approx 1 minutes...."
+echo -e "\e[93mChecking for LIVE domains, this may take some time.\e[0m"
 
 cat $domain/all-domains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > $domain/live-domains.txt
 
-echo "[- Task done -] Results are saved in live-domains.txt"
+echo -e "[- Task done -] \e[92mResults are saved in live-domains.txt\e[0m"
 
 echo "	"
-echo " Fetching all URLs/links of $domain...
-	This will take some time.."
-cat $domain/live-domains.txt | waybackurls > $domain/urls.txt
-echo "[- Task Done -] All urls/links of $domain are saved in urls.txt"
+echo -e "\e[93mFetching all URLs/links of $domain, this may take some time..\e[0m"
+cat $domain/live-domains.txt | waybackurls | sort -u > $domain/urls.txt
+echo -e "[- Task Done -] \e[92mAll urls/links of $domain are saved in urls.txt\e[0m"
 
 echo "	"
-echo "Enumerating all JS files from $domain..
-       This will take approx 1 minute"
+echo -e "\e[93mEnumerating all JS files from $domain, this may take some time\e[0m"
 cat $domain/urls.txt | grep "\.js" | uniq | sort > $domain/js-files.txt
-echo "[- Task Done -] Js files of $domain are saved in js-files.txt"
+echo -e "[- Task Done -] \e[92mJs files of $domain are saved in js-files.txt\e[0m"
 
 echo "	"
-echo "~ All files are saved in $domain directory
-			Happy Hacking :)"
-
+echo -e "\e[95m~ All files are saved in $domain directory\e[0m"
+echo -e "\e[5m			Happy Hacking :)\e[0m"
